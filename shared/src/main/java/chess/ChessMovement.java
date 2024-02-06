b4d07abfeb1577d250b12020023333c5d759f3b1
niclew17 <niclewis17@gmail.com>
2024-02-06 12:49:33 -0700

    Changes to Pawn Class to Pass Invalid Moves

--- shared/src/main/java/chess/ChessMovement.java
+++ shared/src/main/java/chess/ChessMovement.java
@@ -137,13 +137,6 @@ public abstract class ChessMovement {
         }
       }
     }
-    if(!outOfBounds(attackright)) {
-      if (board.getPiece(attackright) != null && !board.getPiece(attackright).getTeamColor().equals(color)) {
-        if (notPromotingPiece(myposition, attackright, color, moveslist)) {
-          moveslist.add(new ChessMove(myposition, attackright, null));
-        }
-      }
-    }
   }
 }
 
