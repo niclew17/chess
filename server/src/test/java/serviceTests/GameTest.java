0e21d88820a2e21eb76c1452f2b20608dc06ee35
niclew17 <niclewis17@gmail.com>
2024-02-27 15:25:13 -0700

    Added game tests

--- server/src/test/java/serviceTests/GameTest.java
+++ server/src/test/java/serviceTests/GameTest.java
@@ -1,20 +1,82 @@
-package java.serviceTests;
+package serviceTests;
 
+import chess.ChessGame;
+import dataAccess.DataAccessException;
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryGameDAO;
+import dataAccess.MemoryUserDAO;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import request.CreateGameRequest;
+import request.JoinGameRequest;
+import request.RegisterRequest;
+import response.CreateGameResponse;
+import response.ListGamesResponse;
+import services.DB;
+import services.Game;
+import services.User;
+
+import java.util.Collection;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+  MemoryUserDAO userDAO = new MemoryUserDAO();
+  MemoryAuthDAO authDAO = new MemoryAuthDAO();
+  MemoryGameDAO gameDAO = new MemoryGameDAO();
+  DB testDB;
+  Game testGame;
+  User testUser;
 
-  @Test
-  void listGames() {
+  @BeforeEach
+  void setup(){
+    testDB = new DB(userDAO, authDAO, gameDAO);
+    testGame = new Game(gameDAO, authDAO);
+    testUser = new User(userDAO, authDAO);
   }
 
   @Test
-  void createGame() {
+  void listGamesCorrect() throws DataAccessException {
+    AuthData auth = testUser.register(new RegisterRequest("nic", "lewis", "hello"));
+    CreateGameResponse response = testGame.createGame(auth.getAuthToken(), new CreateGameRequest("newGame"));
+    assertEquals(gameDAO.listGames(), testGame.listGames(auth.getAuthToken()));
+
+  }
+  @Test
+  void listGamesError() throws DataAccessException{
+    AuthData auth = testUser.register(new RegisterRequest("john", "lewis", "hello"));
+    CreateGameResponse response = testGame.createGame(auth.getAuthToken(), new CreateGameRequest("newGame"));
+    assertThrows(DataAccessException.class, () -> testGame.listGames("12344"), "Error: unauthorized");
+  }
+
+
+  @Test
+  void createGameCorrect() throws DataAccessException {
+    AuthData auth = testUser.register(new RegisterRequest("jane", "lewis", "hello"));
+    assertEquals(new CreateGameResponse(1), testGame.createGame(auth.getAuthToken(), new CreateGameRequest("newGame")));
+  }
+  @Test
+  void createGameError() throws DataAccessException {
+    AuthData auth = testUser.register(new RegisterRequest("jim", "lewis", "hello"));
+    assertThrows(DataAccessException.class, () -> testGame.createGame("1234", new CreateGameRequest("newGame")), "Error: unauthorized");
   }
 
   @Test
-  void joinGame() {
+  void joinGameCorrect() throws DataAccessException{
+    AuthData auth = testUser.register(new RegisterRequest("smith", "lewis", "hello"));
+    CreateGameResponse response = testGame.createGame(auth.getAuthToken(), new CreateGameRequest("newGame"));
+    testGame.joinGame(auth.getAuthToken(), new JoinGameRequest("WHITE", response.gameID()));
+    GameData data = new GameData(response.gameID(), auth.getUsername(), null, "newGame",new ChessGame());
+    assertEquals(data.getWhiteUsername(),gameDAO.getGame(response.gameID()).getWhiteUsername());
+
+  }
+  @Test
+  void joinGameError() throws DataAccessException{
+    AuthData auth = testUser.register(new RegisterRequest("dawn", "lewis", "hello"));
+    CreateGameResponse response = testGame.createGame(auth.getAuthToken(), new CreateGameRequest("newGame"));
+    assertThrows(DataAccessException.class, () -> testGame.joinGame("1234", new JoinGameRequest("WHITE", response.gameID())), "Error: unauthorized");
   }
 }
--- server/src/test/java/serviceTests/UserTest.java
+++ server/src/test/java/serviceTests/UserTest.java
@@ -1,20 +1,61 @@
-package java.serviceTests;
+package serviceTests;
 
+import dataAccess.DataAccessException;
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryGameDAO;
+import dataAccess.MemoryUserDAO;
+import model.AuthData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import request.CreateGameRequest;
+import request.RegisterRequest;
+import services.DB;
+import services.Game;
+import services.User;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
+  MemoryUserDAO userDAO = new MemoryUserDAO();
+  MemoryAuthDAO authDAO = new MemoryAuthDAO();
+  MemoryGameDAO gameDAO = new MemoryGameDAO();
+  DB testDB;
+  User testUser;
+
+  @BeforeEach
+  void setup(){
+    testDB = new DB(userDAO, authDAO, gameDAO);
+    testUser = new User(userDAO, authDAO);
+  }
+
+
   @Test
-  void register() {
+  void registerCorrect() {
+
+  }
+  @Test
+  void registerError() throws DataAccessException{
+    AuthData auth = testUser.register(new RegisterRequest("jim", "lewis", "hello"));
+    testUser.register(new RegisterRequest("jim", "lewis", "hello"));
+    assertThrows(DataAccessException.class, () -> testUser.register(new RegisterRequest("jim", "lewis", "hello")), "Error: already taken");
   }
 
   @Test
-  void login() {
+  void loginCorrect() {
+
+  }
+  @Test
+  void loginError() {
+
   }
 
   @Test
-  void logout() {
+  void logoutCorrect() {
+
+  }
+  @Test
+  void logoutError() {
+
   }
 }
