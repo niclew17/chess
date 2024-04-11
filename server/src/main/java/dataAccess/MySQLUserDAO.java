ae4ccc821d624dfc8482ea45284d0f1fbdc48ba0
niclew17 <niclewis17@gmail.com>
2024-04-11 07:54:16 -0600

    Code Quality Revision 3

--- server/src/main/java/dataAccess/MySQLUserDAO.java
+++ server/src/main/java/dataAccess/MySQLUserDAO.java
@@ -8,9 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 
-import static java.sql.Types.NULL;
-
-public class MySQLUserDAO implements UserDAO {
+public class MySQLUserDAO extends SQLDAO implements UserDAO {
   public MySQLUserDAO() throws DataAccessException {
     configureDatabase();
   }
@@ -74,20 +72,5 @@ public class MySQLUserDAO implements UserDAO {
   public void deleteAll() throws DataAccessException {
     var statement = "TRUNCATE user";
     executeUpdate(statement);
-  }
-  private void executeUpdate(String statement, Object... params) throws DataAccessException {
-    try (var conn = DatabaseManager.getConnection()) {
-      try (var ps = conn.prepareStatement(statement)) {
-        for (var i = 0; i < params.length; i++) {
-          var param = params[i];
-          if (param instanceof String p) ps.setString(i + 1, p);
-          else if (param == null) ps.setNull(i + 1, NULL);
-        }
-        ps.executeUpdate();
-
-      }
-    } catch (SQLException e) {
-      throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
-    }
   }
 }
