17cec0865e2ee312897c8412c5bff7cc4daf5b03
niclew17 <niclewis17@gmail.com>
2024-04-10 22:29:38 -0600

    Changed ToString

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -43,12 +43,7 @@ public class ChessMove {
 
     @Override
     public String toString() {
-        return "ChessMove{" +
-                "startPosition=" + startPosition +
-                ", endPosition=" + endPosition +
-                ", promotionPiece=" + promotionPiece +
-                "}\n"
-                ;
+        return  startPosition + " to " + endPosition + "}\n";
     }
 
     /**
