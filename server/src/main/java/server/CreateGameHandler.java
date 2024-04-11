0535f900fafc36b75979ede8f7479f13b8f2ca13
niclew17 <niclewis17@gmail.com>
2024-04-11 08:28:39 -0600

    Code Quality Revision 7

--- server/src/main/java/server/CreateGameHandler.java
+++ server/src/main/java/server/CreateGameHandler.java
@@ -11,7 +11,7 @@ import services.Game;
 import spark.Request;
 import spark.Response;
 
-public class CreateGameHandler {
+public class CreateGameHandler extends HandlerErrorMethods{
   private final Game service;
 
   public CreateGameHandler(GameDAO gameDAO, AuthDAO authDAO) {
@@ -39,14 +39,4 @@ public class CreateGameHandler {
     }
   }
 
-  private static void sendError(Response res, DataAccessException e) {
-    if (e.getMessage().equals("Error: unauthorized")) {
-       res.status(401);
-     }
-    else {
-       res.status(500);
-     }
-    Message message=new Message(e.getMessage());
-    res.body(new Gson().toJson(message));
-  }
 }
